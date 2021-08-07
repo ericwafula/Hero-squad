c6549c925d8377536131bdc021cf3f4168b6e67f
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-08-07 12:54:27 +0300

    Added Squad getters and setters

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -7,7 +7,7 @@ public class Squad {
     private String mName;
     private String mCause;
     private int mMaxSize;
-    private ArrayList<Squad> mInstances = new ArrayList<Squad>();
+    private static ArrayList<Squad> mInstances = new ArrayList<Squad>();
 
     public Squad(Hero hero, String name, String cause) {
         this.mHero = hero;
@@ -15,5 +15,33 @@ public class Squad {
         this.mCause = cause;
         mInstances.add(this);
         this.mMaxSize = 20;
+    }
+
+    public Hero getHero(){
+        return this.mHero;
+    }
+
+    public String getName(){
+        return this.mName;
+    }
+
+    public String getmCause(){
+        return this.mCause;
+    }
+
+    public static ArrayList<Squad> getAll(){
+        return mInstances;
+    }
+
+    public void setHero(Hero hero){
+        this.mHero = hero;
+    }
+
+    public void setName(String name){
+        this.mName = name;
+    }
+
+    public void setCause(String cause){
+        this.mCause = cause;
     }
 }
