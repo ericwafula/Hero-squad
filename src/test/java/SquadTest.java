eb80b04a502413e05c24d4c29d6b55edc007f271
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-08-07 12:32:52 +0300

    Added Squad instance test

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -3,5 +3,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class SquadTest {
-
+    @Test
+    public void instantiatesSquadCorrectly_true() {
+        Hero hero = new Hero("Eric", 26, "Super Coder", "Patience");
+        Squad squad = new Squad(hero, "Vikings", "Defeat spartans");
+        assertEquals(true, squad instanceof Squad);
+    }
 }
