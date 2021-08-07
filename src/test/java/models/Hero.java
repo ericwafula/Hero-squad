e285e2ab3f2c784495f07e7e90b9ef02677fdae7
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-08-07 09:03:14 +0300

    Created Hero constructor

--- src/test/java/models/Hero.java
+++ src/test/java/models/Hero.java
@@ -1,5 +1,14 @@
 package models;
 
 public class Hero {
-
+    private String mName;
+    private int mAge;
+    private String mSpecialPower;
+    public String mWeakness;
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.mName = name;
+        this.mAge = age;
+        this.mSpecialPower = specialPower;
+        this.mWeakness = weakness;
+    }
 }
