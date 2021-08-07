5d78e244b313780270e09ffc99ca0d79dcab4f1e
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-08-07 09:13:12 +0300

    Added instances array and it's getter

--- src/test/java/models/Hero.java
+++ src/test/java/models/Hero.java
@@ -1,16 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Hero {
     private String mName;
     private int mAge;
     private String mSpecialPower;
     public String mWeakness;
 
+    public static ArrayList<Hero> mInstances = new ArrayList<Hero>();
+
     public Hero(String name, int age, String specialPower, String weakness) {
         this.mName = name;
         this.mAge = age;
         this.mSpecialPower = specialPower;
         this.mWeakness = weakness;
+        mInstances.add(this);
     }
 
     public String getName() {
@@ -43,5 +48,9 @@ public class Hero {
 
     public void setWeakness(String mWeakness) {
         this.mWeakness = mWeakness;
+    }
+
+    public static ArrayList<Hero> getAll() {
+        return mInstances;
     }
 }
