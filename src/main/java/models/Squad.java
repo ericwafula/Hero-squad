a91faa9a3542f4b915847e57d45aaa087208efdb
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-08-07 12:41:10 +0300

    Added squad constructor

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,5 +1,19 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Squad {
-    
+    private Hero mHero;
+    private String mName;
+    private String mCause;
+    private int mMaxSize;
+    private ArrayList<Squad> mInstances = new ArrayList<Squad>();
+
+    public Squad(Hero hero, String name, String cause) {
+        this.mHero = hero;
+        this.mName = name;
+        this.mCause = cause;
+        mInstances.add(this);
+        this.mMaxSize = 20;
+    }
 }
