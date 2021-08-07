e180aa2361091db18d1ae632edd35d263d1aa932
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-08-07 09:05:27 +0300

    Added Hero getters and setters

--- src/test/java/models/Hero.java
+++ src/test/java/models/Hero.java
@@ -5,10 +5,43 @@ public class Hero {
     private int mAge;
     private String mSpecialPower;
     public String mWeakness;
+
     public Hero(String name, int age, String specialPower, String weakness) {
         this.mName = name;
         this.mAge = age;
         this.mSpecialPower = specialPower;
         this.mWeakness = weakness;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public int getAge() {
+        return mAge;
+    }
+
+    public void setAge(int mAge) {
+        this.mAge = mAge;
+    }
+
+    public String getSpecialPower() {
+        return mSpecialPower;
+    }
+
+    public void setSpecialPower(String mSpecialPower) {
+        this.mSpecialPower = mSpecialPower;
+    }
+
+    public String getWeakness() {
+        return mWeakness;
+    }
+
+    public void setWeakness(String mWeakness) {
+        this.mWeakness = mWeakness;
     }
 }
