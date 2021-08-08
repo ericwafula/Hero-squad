ed414bfc6711cef5479e0d3cbda6327c8256f209
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-08-08 10:09:48 +0300

    Added default input functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+
+import models.Hero;
+import models.Squad;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
+            ArrayList<Squad> squads = Squad.getAll();
+            model.put("squads", squads);
+
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -15,11 +23,12 @@ public class App {
             Map<String, Object> model = new HashMap<String, Object>();
             String squad = request.queryParams("squad");
             String cause = request.queryParams("cause");
+            Squad newSquad = new Squad(squad, cause);
             request.session().attribute("squad", squad);
             request.session().attribute("cause", cause);
-            model.put("squad", squad);
-            model.put("cause", cause);
+            model.put("squads", squad);
+            model.put("causes", cause);
             return new ModelAndView(model, "success.hbs");
-        });
+        }, new HandlebarsTemplateEngine());
     }
 }
